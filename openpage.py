546c3fc1060fd62ccbb4a936b87f60d73aa6cab4
Sameehan <sameehan.raichur@gmail.com>
2023-01-21 23:12:45 +0530

    changes made

--- openpage.py
+++ openpage.py
@@ -1,5 +1,5 @@
 from tkinter import *
-#from PIL import Image, ImageTk
+from PIL import Image, ImageTk
 from main import ultra
 import settings
 
@@ -8,8 +8,8 @@ hi.title("welcome")
 hi.geometry(f'{settings.winwidth}x{settings.winheight}')
 hi.configure(bg="light blue")
 
-#image1 = Image.open("images/miner.jpg")
-#test = ImageTk.PhotoImage(image1)
+image1 = Image.open("images/miner.jpg")
+test = ImageTk.PhotoImage(image1)
 label2=Label(hi,text="Minesweeper",font=("Times New Roman",45),bg="light green",fg="purple")
 
 def test2win():
@@ -20,32 +20,31 @@ def test2win():
     hi2.geometry(f'{settings.winwidth}x{settings.winheight}')
     hi2.configure(bg="light blue")
 
-    #image2 = Image.open("images/biggerminer.jpg")
+    image2 = Image.open("images/biggerminer.jpg")
 
-    #resized=image2.resize((400,300),Image.ANTIALIAS)
-
-
-    #test = ImageTk.PhotoImage(resized)
+    resized=image2.resize((400,300),Image.ANTIALIAS)
 
 
+    test = ImageTk.PhotoImage(resized)
 
-    label4=Label(hi2,text="Rules",font=("Times New Roman",35),bg="light green",fg="purple")
-    label5=Label(hi2,text="There are a set number of mines present in the given tiles. Select any tile to begin the game",font=("Times New Roman",14),bg="light green",fg="purple")
-    label6=Label(hi2,text="The goal of the gamr is to not cick on any mines",font=("Times New Roman",14),bg="light green",fg="purple")
-    label7=Label(hi2,text="There are a set number of mines present in the given tiles. Select ant tile to begin teh game",font=("Times New Roman",14),bg="light green",fg="purple")
-    label8=Label(hi2,text="The numbers around certain blocks is tell the number of mines present around the block at any given time",font=("Times New Roman",14),bg="light green",fg="purple")
+    frame4=Frame(hi2,bg="white",width=100,height=100,padx=5,pady=5)
+    label4=Label(frame4,text="Rules",font=("Times New Roman",35),bg="light green",fg="purple")
+    label5=Label(frame4,text="There are a set number of mines present in the given tiles. Select any tile to begin the game",font=("Times New Roman",14),bg="light green",fg="purple")
+    label6=Label(frame4,text="The goal of the game is to not cick on any mines",font=("Times New Roman",14),bg="light green",fg="purple")
+    label7=Label(frame4,text="There are a set number of mines present in the given tiles. Select ant tile to begin teh game",font=("Times New Roman",14),bg="light green",fg="purple")
+    label8=Label(frame4,text="The numbers around certain blocks is tell the number of mines present around the block at any given time",font=("Times New Roman",14),bg="light green",fg="purple")
 
     def mainwin():
         hi2.destroy()
-        #import main
+        import main
         ultra()
-    label3=Button(hi2,text="Click Here To Begin Playing",font=("Times New Roman",20),bg="white",fg="purple", command=mainwin)
+    label3=Button(frame4,text="Click Here To Begin Playing",font=("Times New Roman",20),bg="white",fg="purple", command=mainwin)
 
-    #label1 =Label(image=test)
-    #label1.image = test
-
+    label1 =Label(frame4,image=test)
+    label1.image = test
+    frame4.place(relx=0.5,rely=0.5,anchor="center")
     # Position image
-    #label1.pack()
+    label1.pack()
     label4.pack()
     label5.pack()
     label6.pack()
@@ -54,13 +53,19 @@ def test2win():
     label3.pack()
     hi2.mainloop() 
 
-
-label3=Button(hi,text="Click Here To Play",font=("Times New Roman",20),bg="white",fg="purple", command=test2win)
-
-#label1 =Label(image=test)
-#label1.image = test
-label2.pack()
+frame3=Frame(hi,bg="white",width=100,height=100,padx=5,pady=10)
+label3=Button(frame3,text="Click Here To Play",font=("Times New Roman",20),bg="white",fg="purple", command=test2win)
+label10=Label(frame3,text="Minesweeper",font=("Times New Roman",45),bg="light green",fg="purple")
+label1 =Label(frame3,image=test,padx=5,pady=5)
+label1.image = test
+label10.pack()
 # Position image
-#label1.pack()
+frame3.place(relx=0.5,rely=0.5,anchor="center")
+#frame3.place(relx=5,rely=0.5,anchor="center")
+label1.pack()
 label3.pack()
+#label1.grid(column=1,row=0,padx=10,pady=10,columnspan=2)
+
+#label1.pack()
+#label3.grid(column=1,row=1,padx=10,pady=10,columnspan=2)
 hi.mainloop()
