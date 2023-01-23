abbd9bb3ce1c1470cdd1a6172e4e40cdbae0c73d
“Pradyumn <pradyumnprabhu@gmail.com>
2023-01-23 12:36:20 +0530

    Final final final final

--- closepage.py
+++ closepage.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 import settings
 import sys
+#from openpage import silt
 
 boomimage = Image.open("images/boom.jpg") #importing 1st image
 boom_resized=boomimage.resize((450,300))
@@ -27,21 +28,17 @@ def mega():
 
     frame1=Frame(win,bg="red",width=100,height=500, padx=5, pady=5)
     label1=Label(frame1,image=test,padx=5,pady=5)
-    label2=Label(frame1,text="Game Over",bg="white",fg='black',font=("Courier",45))
-    button1=Button(frame1,text="Play Again",height=1,width=10,font=("Courier",30),bg="white")
+    label2=Label(frame1,text="Game Over",bg="red",fg='black',font=("Courier",45))
     button2=Button(frame1,text="Exit",height=1,width=10,font=("Courier",30),bg="white", command=sys.exit)
-    
 
-    
     label2.grid(column=600,row=300)
     button2.place(x=700,y=225)
-    button1.place(x=400,y=225)
     label1.grid(column=1,row=0,padx=10,pady=10, columnspan=2)
     label2.grid(column=1,row=1,padx=10,pady=10,columnspan=2)
-    button1.grid(column=1, row=2, padx=10,pady=10)
-    button2.grid(column=2, row=2,padx=10,pady=10)
+    button2.grid(column=1, row=2, columnspan=2, padx=10,pady=10)
     frame1.place(relx=0.5, rely=0.5,anchor="center")
     win.mainloop()
+
 
 
 def mini():
@@ -52,19 +49,13 @@ def mini():
     test2= ImageTk.PhotoImage(flagimage_resized)
     win.geometry(f'{settings.winwidth}x{settings.winheight}')
     frame2=Frame(win,bg="green",width=100,height=500, padx=5, pady=5)
-    label3=Label(frame2,text="You have found all the mines!",bg="white",fg='black',font=("Courier",35))
+    label3=Label(frame2,text="You have found all the mines!",bg="green",fg='black',font=("Courier",35))
     label4=Label(frame2,image=test2,padx=5,pady=5)
-    button3=Button(frame2,text="Play Again",height=1,width=10,font=("Courier",30),bg="white")
     button4=Button(frame2,text="Exit",height=1,width=10,font=("Courier",30),bg="white",command=sys.exit)
     
   
     label4.grid(column=1,row=0,padx=10,pady=10, columnspan=2)
     label3.grid(column=1,row=1,padx=10,pady=10,columnspan=2)
-    button3.grid(column=1, row=2, padx=10,pady=10)
-    button4.grid(column=2, row=2,padx=10,pady=10)
+    button4.grid(column=1, row=2, columnspan=2, padx=10,pady=10)
     frame2.place(relx=0.5, rely=0.5,anchor="center")
-<<<<<<< HEAD
     win.mainloop()
-=======
-    win.mainloop()
->>>>>>> f531430e4a3d73b528a31888a2ba7de0c05fc28f
--- openpage.py
+++ openpage.py
@@ -3,79 +3,71 @@ from PIL import Image, ImageTk
 from main import ultra
 import settings
 
-hi = Tk()
-hi.title("welcome")
-hi.geometry(f'{settings.winwidth}x{settings.winheight}')
-hi.configure(bg="light blue")
+def silt():
+    hi = Tk()
+    hi.title("welcome")
+    hi.geometry(f'{settings.winwidth}x{settings.winheight}')
+    hi.configure(bg="light blue")
 
-image1 = Image.open("images/miner.jpg")
-test = ImageTk.PhotoImage(image1)
-label2=Label(hi,text="Minesweeper",font=("Times New Roman",45),bg="light green",fg="purple")
+    image1 = Image.open("images/miner.jpg")
+    test = ImageTk.PhotoImage(image1)
+    label2=Label(hi,text="Minesweeper",font=("Times New Roman",45),bg="light green",fg="purple")
 
-def test2win():
-    hi.destroy()
-    hi2 = Tk()
-    hi2.title("instructions")
-    print(f'{settings.winwidth}x{settings.winheight}')
-    hi2.geometry(f'{settings.winwidth}x{settings.winheight}')
-    hi2.configure(bg="light blue")
+    def test2win():
+        hi.destroy()
+        hi2 = Tk()
+        hi2.title("instructions")
+        print(f'{settings.winwidth}x{settings.winheight}')
+        hi2.geometry(f'{settings.winwidth}x{settings.winheight}')
+        hi2.configure(bg="light blue")
 
-    image2 = Image.open("images/biggerminer.jpg")
+        image2 = Image.open("images/biggerminer.jpg")
 
-    resized=image2.resize((400,300),Image.ANTIALIAS)
-
-
-    test = ImageTk.PhotoImage(resized)
-
-<<<<<<< HEAD
-    frame4=Frame(hi2,bg="white",width=100,height=100,padx=5,pady=5)
-    label4=Label(frame4,text="Rules",font=("Times New Roman",35),bg="light green",fg="purple")
-    label5=Label(frame4,text="There are a set number of mines present in the given tiles. Select any tile to begin the game",font=("Times New Roman",14),bg="light green",fg="purple")
-    label6=Label(frame4,text="The goal of the game is to not cick on any mines",font=("Times New Roman",14),bg="light green",fg="purple")
-    label7=Label(frame4,text="There are a set number of mines present in the given tiles. Select ant tile to begin teh game",font=("Times New Roman",14),bg="light green",fg="purple")
-    label8=Label(frame4,text="The numbers around certain blocks is tell the number of mines present around the block at any given time",font=("Times New Roman",14),bg="light green",fg="purple")
-=======
+        resized=image2.resize((400,300),Image.ANTIALIAS)
 
 
-    label4=Label(hi2,text="Rules",font=("Times New Roman",35),bg="light green",fg="purple")
-    label5=Label(hi2,text="There are a set number of mines present in the given tiles. Select any tile to begin the game",font=("Times New Roman",14),bg="light green",fg="purple")
-    label6=Label(hi2,text="The goal of the game is to not cick on any mines",font=("Times New Roman",14),bg="light green",fg="purple")
-    label7=Label(hi2,text="There are a set number of mines present in the given tiles. Select ant tile to begin teh game",font=("Times New Roman",14),bg="light green",fg="purple")
-    label8=Label(hi2,text="The numbers around certain blocks is tell the number of mines present around the block at any given time",font=("Times New Roman",14),bg="light green",fg="purple")
->>>>>>> f531430e4a3d73b528a31888a2ba7de0c05fc28f
+        test = ImageTk.PhotoImage(resized)
 
-    def mainwin():
-        hi2.destroy()
-        import main
-        ultra()
-    label3=Button(frame4,text="Click Here To Begin Playing",font=("Times New Roman",20),bg="white",fg="purple", command=mainwin)
+        frame4=Frame(hi2,bg="light blue",width=100,height=100,padx=5,pady=5)
+        label4=Label(frame4,text="Rules",font=("Times New Roman",35),bg="light green",fg="purple")
+        label5=Label(frame4,text="There are a set number of mines present in the given tiles. Select any tile to begin the game",font=("Times New Roman",14),bg="light green",fg="purple")
+        label6=Label(frame4,text="The goal of the game is to not cick on any mines",font=("Times New Roman",14),bg="light green",fg="purple")
+        label7=Label(frame4,text="There are a set number of mines present in the given tiles. Select ant tile to begin teh game",font=("Times New Roman",14),bg="light green",fg="purple")
+        label8=Label(frame4,text="The numbers around certain blocks is tell the number of mines present around the block at any given time",font=("Times New Roman",14),bg="light green",fg="purple")
 
-    label1 =Label(frame4,image=test)
+        def mainwin():
+            hi2.destroy()
+            import main
+            ultra()
+        label3=Button(frame4,text="Click Here To Begin Playing",font=("Times New Roman",20),bg="white",fg="purple", command=mainwin)
+
+        label1 =Label(frame4,image=test)
+        label1.image = test
+        frame4.place(relx=0.5,rely=0.5,anchor="center")
+        # Position image
+        label1.pack()
+        label4.pack()
+        label5.pack()
+        label6.pack()
+        label7.pack()
+        label8.pack()
+        label3.pack()
+        hi2.mainloop() 
+
+    frame3=Frame(hi,bg="white",width=100,height=100,padx=5,pady=10)
+    label3=Button(frame3,text="Click Here To Play",font=("Times New Roman",20),bg="white",fg="purple", command=test2win)
+    label10=Label(frame3,text="Minesweeper",font=("Times New Roman",45),bg="light green",fg="purple")
+    label1 =Label(frame3,image=test,padx=5,pady=5)
     label1.image = test
-    frame4.place(relx=0.5,rely=0.5,anchor="center")
+    label10.pack()
     # Position image
+    frame3.place(relx=0.5,rely=0.5,anchor="center")
+    #frame3.place(relx=5,rely=0.5,anchor="center")
     label1.pack()
-    label4.pack()
-    label5.pack()
-    label6.pack()
-    label7.pack()
-    label8.pack()
     label3.pack()
-    hi2.mainloop() 
+    #label1.grid(column=1,row=0,padx=10,pady=10,columnspan=2)
 
-frame3=Frame(hi,bg="white",width=100,height=100,padx=5,pady=10)
-label3=Button(frame3,text="Click Here To Play",font=("Times New Roman",20),bg="white",fg="purple", command=test2win)
-label10=Label(frame3,text="Minesweeper",font=("Times New Roman",45),bg="light green",fg="purple")
-label1 =Label(frame3,image=test,padx=5,pady=5)
-label1.image = test
-label10.pack()
-# Position image
-frame3.place(relx=0.5,rely=0.5,anchor="center")
-#frame3.place(relx=5,rely=0.5,anchor="center")
-label1.pack()
-label3.pack()
-#label1.grid(column=1,row=0,padx=10,pady=10,columnspan=2)
-
-#label1.pack()
-#label3.grid(column=1,row=1,padx=10,pady=10,columnspan=2)
-hi.mainloop()
+    #label1.pack()
+    #label3.grid(column=1,row=1,padx=10,pady=10,columnspan=2)
+    hi.mainloop()
+silt()
